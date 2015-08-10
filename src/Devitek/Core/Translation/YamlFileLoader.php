f6e8ceec6d4c226c1d1f9a1415d1d44bc3b39b7b
Paris Paraskeva <paris@unitedworx.com>
2015-08-11 00:34:33 +0300

    Update YamlFileLoader.php for laravel 5.1 compatibility
    didnt take long to figure out and update this :) 
    
    since there are barking changes you might need to add this as aa different version e.g version 2.0 if you want to keep version 1.x for laravel 5.0

--- src/Devitek/Core/Translation/YamlFileLoader.php
+++ src/Devitek/Core/Translation/YamlFileLoader.php
@@ -5,20 +5,71 @@ use Illuminate\Translation\FileLoader;
 use Symfony\Component\Yaml\Parser;
 use File;
 
-class YamlFileLoader extends FileLoader
+use Illuminate\Translation\LoaderInterface;
+
+class YamlFileLoader implements LoaderInterface
 {
 
+    /**
+     * The filesystem instance.
+     *
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
+
+    /**
+     * The default path for the loader.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * All of the namespace hints.
+     *
+     * @var array
+     */
+    protected $hints = [];
+
+    /**
+     * Create a new file loader instance.
+     *
+     * @param  \Illuminate\Filesystem\Filesystem  $files
+     * @param  string  $path
+     * @return void
+     */
+    public function __construct(Filesystem $files, $path)
+    {
+        $this->path = $path;
+        $this->files = $files;
+    }
+    
+    
+    /**
+     * Get Allowed Translation File Extensions.
+     *
+     * @return array
+     */
     protected function getAllowedFileExtensions()
     {
         return ['php', 'yml', 'yaml'];
     }
     
     
+    /**
+     * Load a local namespaced translation group for overrides.
+     *
+     * @param  array  $lines
+     * @param  string  $locale
+     * @param  string  $group
+     * @param  string  $namespace
+     * @return array
+     */
 	protected function loadNamespaceOverrides(array $lines, $locale, $group, $namespace)
 	{
 		foreach ($this->getAllowedFileExtensions() as $extension) {
 			
-			$file = "{$this->path}/packages/{$locale}/{$namespace}/{$group}.{$extension}";
+			$file = "{$this->path}/vendor/{$namespace}/{$locale}/{$group}.{$extension}";
 	
 			if ($this->files->exists($file))
 			{
@@ -32,6 +83,14 @@ class YamlFileLoader extends FileLoader
 	}
 
 
+    /**
+     * Load a locale from a given path.
+     *
+     * @param  string  $path
+     * @param  string  $locale
+     * @param  string  $group
+     * @return array
+     */
 	protected function loadPath($path, $locale, $group)
 	{
 		
@@ -49,6 +108,13 @@ class YamlFileLoader extends FileLoader
 	}
 
 
+     /**
+     * require reqular php file or parse yaml before loading it
+     *
+     * @param  string  $extension
+     * @param  string  $file
+     * @return array
+     */
     protected function parseContent($extension, $file)
     {
         $content = null;
@@ -66,6 +132,13 @@ class YamlFileLoader extends FileLoader
         return $content;
     }
 
+
+     /**
+     * Parse a Yaml file and return as a php array or load from cache.
+     *
+     * @param  string  $file
+     * @return array
+     */
     protected function parseYamlOrLoadFromCache($file)
     {
 
@@ -89,6 +162,57 @@ class YamlFileLoader extends FileLoader
         }
 
         return $content;
+    }
+    
+    
+     /**
+     * Load the messages for the given locale.
+     *
+     * @param  string  $locale
+     * @param  string  $group
+     * @param  string  $namespace
+     * @return array
+     */
+    public function load($locale, $group, $namespace = null)
+    {
+        if (is_null($namespace) || $namespace == '*') {
+            return $this->loadPath($this->path, $locale, $group);
+        }
+
+        return $this->loadNamespaced($locale, $group, $namespace);
+    }
+	
+	
+	/**
+     * Load a namespaced translation group.
+     *
+     * @param  string  $locale
+     * @param  string  $group
+     * @param  string  $namespace
+     * @return array
+     */
+    protected function loadNamespaced($locale, $group, $namespace)
+    {
+        if (isset($this->hints[$namespace])) {
+            $lines = $this->loadPath($this->hints[$namespace], $locale, $group);
+
+            return $this->loadNamespaceOverrides($lines, $locale, $group, $namespace);
+        }
+
+        return [];
+    }
+    
+    
+     /**
+     * Add a new namespace to the loader.
+     *
+     * @param  string  $namespace
+     * @param  string  $hint
+     * @return void
+     */    
+    public function addNamespace($namespace, $hint)
+    {
+        $this->hints[$namespace] = $hint;
     }
     
 }
