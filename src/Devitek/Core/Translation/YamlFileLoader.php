81b3eaea02d48a6cb35e9cb6c5202b7d24be2f1e
Michiel Kempen <kempenmichiel@gmail.com>
2017-01-25 03:06:06 +0100

    add support for Laravel 5.4
    In Laravel 5.4 a new method was added to
    Illuminate\Translation\LoaderInterface which had to be implemented by
    the YamlFileLoader.

--- src/Devitek/Core/Translation/YamlFileLoader.php
+++ src/Devitek/Core/Translation/YamlFileLoader.php
@@ -214,4 +214,14 @@ class YamlFileLoader implements LoaderInterface
         $this->hints[$namespace] = $hint;
     }
 
+
+    /**
+     * Get an array of all the registered namespaces.
+     *
+     * @return array
+     */
+    public function namespaces()
+    {
+        return $this->hints;
+    }
 }
