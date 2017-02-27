1ae9cfb28c217598fe0d61ff4fd45679c75a8cb9
Thomas Sieffert <tsieffert@digitaleo.com>
2017-02-27 16:16:55 +0100

    Merge #10 and Fix #9 - Add support for "dot notation"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## [3.0.0] - 2017-02-27
+### Fixed
+- Add support to "dot notation"
+
 ## [2.1.1] - 2017-01-25
 ### Fixed
 - Update composer.lock
--- src/Devitek/Core/Translation/YamlFileLoader.php
+++ src/Devitek/Core/Translation/YamlFileLoader.php
@@ -1,18 +1,19 @@
-<?php namespace Devitek\Core\Translation;
+<?php
+
+namespace Devitek\Core\Translation;
 
 use File;
-use Illuminate\Support\Arr;
-use Symfony\Component\Yaml\Parser;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Arr;
 use Illuminate\Translation\LoaderInterface;
+use Symfony\Component\Yaml\Parser;
 
 class YamlFileLoader implements LoaderInterface
 {
-
     /**
      * The filesystem instance.
      *
-     * @var \Illuminate\Filesystem\Filesystem
+     * @var Filesystem
      */
     protected $files;
 
@@ -33,16 +34,14 @@ class YamlFileLoader implements LoaderInterface
     /**
      * Create a new file loader instance.
      *
-     * @param  \Illuminate\Filesystem\Filesystem  $files
-     * @param  string  $path
-     * @return void
+     * @param  Filesystem $files
+     * @param  string     $path
      */
     public function __construct(Filesystem $files, $path)
     {
-        $this->path = $path;
+        $this->path  = $path;
         $this->files = $files;
     }
-
 
     /**
      * Get Allowed Translation File Extensions.
@@ -54,64 +53,55 @@ class YamlFileLoader implements LoaderInterface
         return ['php', 'yml', 'yaml'];
     }
 
-
     /**
      * Load a local namespaced translation group for overrides.
      *
      * @param  array  $lines
-     * @param  string  $locale
-     * @param  string  $group
-     * @param  string  $namespace
+     * @param  string $locale
+     * @param  string $group
+     * @param  string $namespace
+     *
      * @return array
      */
-	protected function loadNamespaceOverrides(array $lines, $locale, $group, $namespace)
-	{
-		foreach ($this->getAllowedFileExtensions() as $extension) {
+    protected function loadNamespaceOverrides(array $lines, $locale, $group, $namespace)
+    {
+        foreach ($this->getAllowedFileExtensions() as $extension) {
+            $file = "{$this->path}/vendor/{$namespace}/{$locale}/{$group}.{$extension}";
 
-			$file = "{$this->path}/vendor/{$namespace}/{$locale}/{$group}.{$extension}";
+            if ($this->files->exists($file)) {
+                return array_replace_recursive($lines, $this->parseContent($extension, $file));
+            }
+        }
 
-			if ($this->files->exists($file))
-			{
-				return array_replace_recursive($lines, $this->parseContent($extension, $file));
-			}
-
-		}
-
-		return $lines;
-
-	}
-
+        return $lines;
+    }
 
     /**
      * Load a locale from a given path.
      *
-     * @param  string  $path
-     * @param  string  $locale
-     * @param  string  $group
+     * @param  string $path
+     * @param  string $locale
+     * @param  string $group
+     *
      * @return array
      */
-	protected function loadPath($path, $locale, $group)
-	{
+    protected function loadPath($path, $locale, $group)
+    {
+        foreach ($this->getAllowedFileExtensions() as $extension) {
+            if ($this->files->exists($full = "{$path}/{$locale}/{$group}.{$extension}")) {
+                return $this->parseContent($extension, $full);
+            }
+        }
 
-		foreach ($this->getAllowedFileExtensions() as $extension) {
+        return [];
+    }
 
-			if ($this->files->exists($full = "{$path}/{$locale}/{$group}.{$extension}"))
-			{
-				return $this->parseContent($extension, $full);
-			}
-
-		}
-
-		return array();
-
-	}
-
-
-     /**
+    /**
      * require reqular php file or parse yaml before loading it
      *
-     * @param  string  $extension
-     * @param  string  $file
+     * @param  string $extension
+     * @param  string $file
+     *
      * @return array
      */
     protected function parseContent($extension, $file)
@@ -131,45 +121,41 @@ class YamlFileLoader implements LoaderInterface
         return Arr::dot($content);
     }
 
-
-     /**
+    /**
      * Parse a Yaml file and return as a php array or load from cache.
      *
-     * @param  string  $file
+     * @param  string $file
+     *
      * @return array
      */
     protected function parseYamlOrLoadFromCache($file)
     {
-
-		$cachedir = storage_path() . '/yaml-translation/';
-
-	    $cachefile = $cachedir . 'cache.' . md5($file) . '.php';
+        $cachedir  = storage_path() . '/yaml-translation/';
+        $cachefile = $cachedir . 'cache.' . md5($file) . '.php';
 
         if (@filemtime($cachefile) < filemtime($file)) {
-
             $parser  = new Parser();
             $content = null === ($yaml = $parser->parse(file_get_contents($file))) ? [] : $yaml;
-            if ( !file_exists($cachedir) ){
-	            @mkdir($cachedir, 0755);
-	        }
+
+            if (! file_exists($cachedir)) {
+                @mkdir($cachedir, 0755);
+            }
+
             file_put_contents($cachefile, "<?php" . PHP_EOL . PHP_EOL . "return " . var_export($content, true) . ";");
-
         } else {
-
             $content = require $cachefile;
-
         }
 
         return $content;
     }
 
-
-     /**
+    /**
      * Load the messages for the given locale.
      *
-     * @param  string  $locale
-     * @param  string  $group
-     * @param  string  $namespace
+     * @param  string $locale
+     * @param  string $group
+     * @param  string $namespace
+     *
      * @return array
      */
     public function load($locale, $group, $namespace = null)
@@ -181,13 +167,13 @@ class YamlFileLoader implements LoaderInterface
         return $this->loadNamespaced($locale, $group, $namespace);
     }
 
-
-	/**
+    /**
      * Load a namespaced translation group.
      *
-     * @param  string  $locale
-     * @param  string  $group
-     * @param  string  $namespace
+     * @param  string $locale
+     * @param  string $group
+     * @param  string $namespace
+     *
      * @return array
      */
     protected function loadNamespaced($locale, $group, $namespace)
@@ -201,19 +187,18 @@ class YamlFileLoader implements LoaderInterface
         return [];
     }
 
-
-     /**
+    /**
      * Add a new namespace to the loader.
      *
-     * @param  string  $namespace
-     * @param  string  $hint
+     * @param  string $namespace
+     * @param  string $hint
+     *
      * @return void
      */
     public function addNamespace($namespace, $hint)
     {
         $this->hints[$namespace] = $hint;
     }
-
 
     /**
      * Get an array of all the registered namespaces.
