6a6f789efebef2dbb4236ace8f984a0b35dc168e
Marco Raddatz <info@marcoraddatz.com>
2017-09-05 13:37:18 +0200

    Added default config

--- src/Devitek/Core/Translation/TranslationServiceProvider.php
+++ src/Devitek/Core/Translation/TranslationServiceProvider.php
@@ -6,11 +6,33 @@ use Illuminate\Translation\TranslationServiceProvider as IlluminateTranslationSe
 
 class TranslationServiceProvider extends IlluminateTranslationServiceProvider
 {
-	protected function registerLoader()
-	{
-		$this->app->singleton('translation.loader', function($app)
-		{
-			return new YamlFileLoader($app['files'], $app['path.lang']);
-		});
-	}
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../../config/yaml-translation.php' => config_path('yaml-translation.php', 'yaml-translation'),
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../../config/yaml-translation.php', 'yaml-translation'
+        );
+    }
+
+    /**
+     * @return void
+     */
+    protected function registerLoader()
+    {
+        $this->app->singleton('translation.loader', function ($app) {
+            return new YamlFileLoader($app[ 'files' ], $app[ 'path.lang' ]);
+        });
+    }
 }
---
+++ src/config/yaml-translation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    // Shall this package use the array_dot schema or Laravel?
+    // If true, this will flatten all array contents into a string.
+    'dot_syntax' =>  true
+];
